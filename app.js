835108bc9497073d7fe6f0721077410eb3719e3b
Ross Hadden <rosshadden@gmail.com>
2013-11-12 14:11:54 -0500

    Got sockets working.

---
+++ app.js
@@ -0,0 +1,8 @@
+server.io.sockets.on("connection", function(socket) {
+	console.info("Socket connection established.");
+
+	socket.emit("welcome", { hello: "world" });
+	socket.on("my other event", function(data) {
+		log("other event", data);
+	});
+});
--- public/js/main.js
+++ public/js/main.js
@@ -2,4 +2,10 @@
 	"use strict";
 
 	// This is an app-wide script, included in layout.html.
+	var socket = io.connect("http://localhost");
+
+	socket.on("welcome", function(data) {
+		console.log("welcome", data);
+		socket.emit("my other event", { my: "data" });
+	});
 })();
