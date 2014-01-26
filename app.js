ee21d663a2a3d14c87025bbaa30ca1092bbec573
Ross Hadden <rosshadden@gmail.com>
2014-01-25 21:40:22 -0500

    Added cachebuster to config.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-server.io.sockets.on("connection", function(socket) {
-	console.info("Socket connection established.");
+if (app.config.useSockets) {
+	server.io.sockets.on("connection", function(socket) {
+		console.info("Socket connection established.");
 
-	socket.emit("welcome", { hello: "world" });
-	socket.on("my other event", function(data) {
-		log("other event", data);
+		socket.emit("welcome", { hello: "world" });
+		socket.on("my other event", function(data) {
+			log("other event", data);
+		});
 	});
-});
+}
