88d8931266c17edba86499aa37cc07f2d500a6d7
Sean Clark <sean@square-bracket.com>
2014-02-12 01:17:45 -0500

    pretty face

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,8 @@
 (function() {
 	"use strict";
-
-	// This is an app-wide script, included in layout.html.
-	var socket = io.connect("http://localhost");
-
-	socket.on("welcome", function(data) {
-		console.log("welcome", data);
-		socket.emit("my other event", { my: "data" });
-	});
+	
+	console.log("woohoo");
+	console.log("Look at the CS variable for debugging");
+	console.log(CS);
+	
 })();
