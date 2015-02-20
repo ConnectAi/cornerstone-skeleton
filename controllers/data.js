76f73343964f07251dfb785f1aff90562c36fb2d
Ross Hadden <rosshadden@gmail.com>
2015-02-19 22:34:32 -0500

    So much better testing organization.

--- controllers/data.js
+++ controllers/data.js
@@ -1,8 +1,8 @@
-var fs = require('fs');
-var request = require('request');
+let fs = require('fs');
+let request = require('request');
 
 
-var query = function(input) {
+let query = function(input) {
 	return new Promise(function(resolve, reject) {
 		app.db.collection('user')
 			.find({ social: 'facebook' })
@@ -12,7 +12,7 @@ var query = function(input) {
 	});
 };
 
-var toHTML = function(results) {
+let toHTML = function(results) {
 	return results.map(function(item) {
 		return `
 			<li>
@@ -29,7 +29,7 @@ var toHTML = function(results) {
 	}).join('<br>');
 };
 
-var read = function(input) {
+let read = function(input) {
 	return new Promise(function(resolve, reject) {
 		request('http://www.gutenberg.org/files/2701/old/moby10b.txt', function(err, response, body) {
 			resolve('body');
@@ -37,7 +37,7 @@ var read = function(input) {
 	});
 };
 
-var makeTimeout = function(n = 1000) {
+let makeTimeout = function(n = 1000) {
 	return new Promise(function(resolve, reject) {
 		setTimeout(function() {
 			resolve(n);
@@ -46,73 +46,76 @@ var makeTimeout = function(n = 1000) {
 };
 
 
+let tests = {
+	timeout: function(variant) {
+		let time;
+
+		switch (variant) {
+			case '1':
+				time = 0;
+				break;
+			case '2':
+				time = 2000;
+				break;
+			case '3':
+				time = 10000;
+				break;
+		}
+
+		return [ makeTimeout(time) ];
+	}
+};
+
+
 module.exports = {
-	'get/:n?'(req, res, next, n) {
-		var fn = makeTimeout;
-		if (n === 'read') fn = read;
-		if (n === 'query') fn = query;
-		if (n === 'query:html') fn = query;
 
-		fn(n)
-			.then(function(data) {
-				if (n === 'query:html') {
-					data = toHTML(data);
-				}
-				res.json(data);
-			});
-	},
+	"server/:test/:variant"(req, res, next, test, variant) {
+		res.locals.method = 'server';
 
-	ajax(req, res) {
-		var start = Date.now();
-		res.view({
-			start
-		});
-	},
+		let start = Date.now();
+		let promises = tests[test](variant);
 
-	server(req, res) {
-		var start = Date.now();
-
-		Promise.all([
-			read('read'),
-			makeTimeout(5e2),
-			makeTimeout(1e1),
-			makeTimeout(),
-			query(),
-			read(),
-			makeTimeout(2e3),
-			read(),
-			query().then(toHTML),
-		])
-		.then(function([data0, data1, data2, data3, data4, data5, data6, data7, data8, data9]) {
+		Promise.all(promises)
+		.then(function(data) {
 			res.view({
-				data0,
-				data1,
-				data2,
-				data3,
-				data4,
-				data5,
-				data6,
-				data7,
-				data8,
+				data,
 				start
 			});
 		});
 	},
 
-	stream(req, res) {
-		var start = Date.now();
+
+	"ajax/:test/:variant"(req, res) {
+		res.locals.method = 'ajax';
+
+		let start = Date.now();
 
 		res.view({
-			data0: read(),
-			data1: makeTimeout(5e2),
-			data2: makeTimeout(1e1),
-			data3: makeTimeout(),
-			data4: query(),
-			data5: read(),
-			data6: makeTimeout(2e3),
-			data7: read(),
-			data8: query().then(toHTML),
+			start
+		});
+	},
+
+
+	'get/:test/:variant?'(req, res, next, test, variant) {
+		let promises = tests[test](variant);
+
+		Promise.all(promises)
+			.then(function(data) {
+				res.json(data);
+			});
+	},
+
+
+	"stream/:test/:variant"(req, res, next, test, variant) {
+		res.locals.method = 'stream';
+
+		let start = Date.now();
+		let promises = tests[test](variant);
+
+		res.view({
+			promises,
 			start
 		});
 	}
+
 };
